a3c62096ca65dae1ddda31b9ba0babac3aa1e584
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-03-20 12:02:52 +0000

    first commit from codesanbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-orange-400">
-      <p className="text-white">Holi</p>
+      <p className="text-white">Saludos desde Codesanbox</p>
     </div>
   );
 }
