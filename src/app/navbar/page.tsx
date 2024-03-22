d4c42fb4edeae687d4963962364612be70c020d9
arturhonores <61164887+arturhonores@users.noreply.github.com>
2024-03-22 21:00:15 +0000

    second commit

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <p className="bg-blue-500">navbar is </p>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Navbar from "./navbar/page";
 export default function Home() {
   return (
-    <div className="bg-orange-400">
-      <p className="text-white">Saludos desde Codesanbox</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
