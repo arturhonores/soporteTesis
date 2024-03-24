4fe15b5828503b1acb19feefca509d5a10899ada
arturhonores <luis.arturhonores@gmail.com>
2024-03-24 22:16:03 +0100

    video

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -1,4 +1,16 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="max-w-7xl m-auto overflow-x-hidden">
+      <div className="">
+        <video
+          src="/video-hero.mp4"
+          autoPlay
+          loop
+          muted
+          className="w-full"
+        ></video>
+      </div>
+    </section>
+  );
 };
 export default Hero;
