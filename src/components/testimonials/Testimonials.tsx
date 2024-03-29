a664baab978605b4a317779afcc2eec5c2ce21ac
arturhonores <luis.arturhonores@gmail.com>
2024-03-29 16:21:47 +0100

    testimonials done

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,6 +1,8 @@
 "use client";
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
 
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -16,6 +18,7 @@ interface TestimonialsClients {
   comment: string;
   stars: number;
   date: string;
+  avatar: string;
 }
 
 const Testimonials = () => {
@@ -26,48 +29,53 @@ const Testimonials = () => {
     {
       name: "Gloria Reluz Flores",
       comment:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic reiciendis molestias tenetur totam delectus velit.",
+        "Trabajar con Jean es interactuar con un profesional competente que ayuda a comprender los procesos en función de la calidad. En tal sentido recomiendo sus servicios en la búsqueda de resultados sobresalientes",
       stars: 4,
       date: "20-10-23",
+      avatar: "/avatar1.jpg",
     },
     {
       name: "Mikel Honores Agapito",
       comment:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic reiciendis molestias tenetur totam delectus velit.",
+        "He laborado con Jean, es un profesional excepcional, tiene mucha experiencia en educación, metodología y didáctica de la investigación. Estamos seguros que tu tiempo e inversión están en buenas manos.",
       stars: 5,
       date: "20-10-23",
+      avatar: "/avatar3.jpg",
     },
     {
       name: "Verónica Agapito Reluz",
       comment:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic reiciendis molestias tenetur totam delectus velit.",
-      stars: 4,
+        "Conocí a Jean en una escuela de posgrado, es muy comprometido en lo que se propone, conoce como funciona todo el proceso académico.",
+      stars: 5,
       date: "20-10-23",
+      avatar: "/avatar2.jpg",
     },
     {
       name: "Roberto Ayasta Reluz",
       comment:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic reiciendis molestias tenetur totam delectus velit.",
+        "Es indiscutible su profesionalismo; mi tesis cumplió al 100% con las Normas APA gracias a ellos.",
       stars: 4,
       date: "20-10-23",
+      avatar: "/avatar5.jpg",
     },
     {
       name: "Martha Mendoza Portuguez",
       comment:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic reiciendis molestias tenetur totam delectus velit.",
-      stars: 4,
+        "Súper recomendado, todo fue a tiempo y forma, y siempre mantienen contacto contigo lo que crea más confianza 🙂",
+      stars: 5,
       date: "20-10-23",
+      avatar: "/avatar6.jpg",
     },
   ];
 
   return (
-    <section className="bg-zinc-100 py-16x">
+    <section className="bg-zinc-100 py-16">
       <section className="max-w-7xl m-auto px-4">
-        <div className="flex flex-col md:flex-row h-96 items-center border border-orange-300">
-          <div className="bg-red-400 border-4 border-yellow-500 w-full md:w-1/2 flex justify-center">
+        <div className="flex flex-col md:flex-row h-96 justify-between md:items-center">
+          <div className="w-full md:w-1/2 flex justify-center order-2 md:order-1">
             <Carousel
               opts={{
-                align: "start",
+                align: "center",
               }}
               orientation="vertical"
               className="w-full max-w-md"
@@ -75,7 +83,7 @@ const Testimonials = () => {
               // onMouseEnter={plugin.current.stop}
               // onMouseLeave={plugin.current.reset}
             >
-              <CarouselContent className="-mt-1 h-[300px]">
+              <CarouselContent className="-mt-1 h-[220px] md:h-[300px]">
                 {testimonios.map((testimonio, index) => (
                   <CarouselItem
                     key={index}
@@ -83,9 +91,33 @@ const Testimonials = () => {
                   >
                     <div className="p-1">
                       <Card>
-                        <CardContent className="flex items-center p-6">
-                          <h6>{testimonio.name}</h6>
-                          <p></p>
+                        <CardContent className="flex flex-col items-start p-6 space-y-2">
+                          <div className="flex items-center gap-x-2">
+                            <Image
+                              src={testimonio.avatar}
+                              alt="avatar"
+                              width={50}
+                              height={50}
+                              className="rounded-full"
+                            ></Image>
+                            <div>
+                              <h6 className="text-xs font-bold">
+                                {testimonio.name}
+                              </h6>
+                              <p className="text-xs text-gray-500">
+                                {testimonio.date}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="flex">
+                            {[...Array(testimonio.stars)].map((_, i) => (
+                              <FaStar
+                                key={i}
+                                className="text-yellow-500"
+                              />
+                            ))}
+                          </div>
+                          <p className="text-sm">{testimonio.comment}</p>
                         </CardContent>
                       </Card>
                     </div>
@@ -96,9 +128,9 @@ const Testimonials = () => {
               <CarouselNext />
             </Carousel>
           </div>
-          <div className="bg-green-500 w-full md:w-1/2 border-4 border-cyan-600">
-            <h3 className="text-3xl font-black text-center">
-              Testimonios que nos llegan de orgullo
+          <div className="w-full md:w-1/2 order-1 md:order-2">
+            <h3 className="text-3xl font-black m-auto max-w-sm text-center">
+              Testimonios que nos llenan de orgullo
             </h3>
           </div>
         </div>
